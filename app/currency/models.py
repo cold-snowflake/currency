b61571f722dea2f87bed67de93c73b675f035e5b
cold-snowflake <cold.snowflake01@gmail.com>
2023-07-11 20:59:00 +0200

    homework8

--- app/currency/models.py
+++ app/currency/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Rate(models.Model):
     buy = models.DecimalField(max_digits=6, decimal_places=2)
     sell = models.DecimalField(max_digits=6, decimal_places=2)
-    created = models.DateTimeField()
+    created = models.DateTimeField(auto_now_add=True)
     currency = models.CharField(max_length=3)
     source = models.CharField(max_length=68)
 
@@ -13,3 +13,8 @@ class ContactUs(models.Model):
     email_from = models.EmailField(blank=False)
     subject = models.CharField(max_length=25)
     message = models.CharField(max_length=250)
+
+
+class Source(models.Model):
+    source_url = models.CharField(max_length=255)
+    name = models.CharField(max_length=64)
--- app/currency/views.py
+++ app/currency/views.py
@@ -1,5 +1,9 @@
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
-from currency.models import Rate, ContactUs
+from django.shortcuts import get_object_or_404
+
+from currency.models import Rate, ContactUs, Source
+from currency.forms import RateForm, SourceForm
 
 
 def rate_list(request):
@@ -10,9 +14,120 @@ def rate_list(request):
     return render(request, 'rate_list.html', context)
 
 
+def rate_create(request):
+    if request.method == 'POST':
+        form = RateForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return HttpResponseRedirect('/rate/list')
+
+    elif request.method == 'GET':
+        form = RateForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'rate_create.html', context)
+
+
+def rate_update(request, pk):
+    rate = get_object_or_404(Rate, pk=pk)
+    if request.method == 'POST':
+        form = RateForm(request.POST, instance=rate)
+        if form.is_valid():
+            form.save()
+        return HttpResponseRedirect('/rate/list')
+    elif request.method == 'GET':
+        form = RateForm(instance=rate)
+    context = {
+        'form': form
+    }
+    return render(request, 'rate_update.html', context)
+
+
+def rate_delete(request, pk):
+    rate = get_object_or_404(Rate, pk=pk)
+
+    if request.method == 'GET':
+        context = {
+            'object': rate
+        }
+        return render(request, 'rate_delete.html', context)
+    elif request.method == 'POST':
+        rate.delete()
+        return HttpResponseRedirect('/rate/list/')
+
+
+def rate_details(request, pk):
+    rate = get_object_or_404(Rate, pk=pk)
+    context = {
+        'rate': rate
+    }
+    return render(request, 'rate_details.html', context)
+
+
 def contact_list(request):
     contacts = ContactUs.objects.all()
     context = {
         'contacts': contacts
     }
     return render(request, 'contact_us.html', context)
+
+
+def source_list(request):
+    source = Source.objects.all()
+    context = {
+        'source': source
+    }
+    return render(request, 'source_list.html', context)
+
+
+def source_create(request):
+    if request.method == 'POST':
+        form = SourceForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return HttpResponseRedirect('/source/list')
+
+    elif request.method == 'GET':
+        form = SourceForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'source_create.html', context)
+
+
+def source_update(request, pk):
+    source = get_object_or_404(Source, pk=pk)
+    if request.method == 'POST':
+        form = SourceForm(request.POST, instance=source)
+        if form.is_valid():
+            form.save()
+        return HttpResponseRedirect('/source/list/')
+
+    elif request.method == 'GET':
+        form = SourceForm(instance=source)
+    context = {
+        'form': form
+    }
+    return render(request, 'source_update.html', context)
+
+
+def source_delete(request, pk):
+    source = get_object_or_404(Source, pk=pk)
+    if request.method == 'GET':
+        context = {
+           'object': source
+        }
+        return render(request, 'source_delete.html', context)
+
+    elif request.method == 'POST':
+        source.delete()
+        return HttpResponseRedirect('/source/list')
+
+
+def source_details(request, pk):
+    source = get_object_or_404(Source, pk=pk)
+    context = {
+        'source': source
+    }
+    return render(request, 'source_details.html', context)
