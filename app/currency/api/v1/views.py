51861213ca727b99a94094db6cc23fc7c7a4be0a
cold-snowflake <cold.snowflake01@gmail.com>
2023-09-20 11:38:40 +0200

    rate paginations and filters

--- app/currency/api/v1/views.py
+++ app/currency/api/v1/views.py
@@ -3,11 +3,23 @@ from rest_framework.renderers import JSONRenderer
 from rest_framework_xml.renderers import XMLRenderer
 from rest_framework_yaml.renderers import YAMLRenderer
 
+from django_filters import rest_framework as filters
+from rest_framework import filters as rest_framework_filters
+
 from currency.models import Rate
-from app.currency.api.v1.serializers import RateSerializer
+from currency.api.v1.serializers import RateSerializer
+from currency.api.v1.paginations import RatePagination
+from currency.api.v1.filters import RateFilter
 
 
 class RatesViewSet(viewsets.ModelViewSet):
     queryset = Rate.objects.all().order_by('-created')
     serializer_class = RateSerializer
     renderer_classes = (JSONRenderer, XMLRenderer, YAMLRenderer)
+    pagination_class = RatePagination
+    filter_backends = (
+        filters.DjangoFilterBackend,
+        rest_framework_filters.OrderingFilter,
+    )
+    filterset_class = RateFilter
+    ordering_fields = ('buy', 'sell', 'created')
