f00917a7c095c93add90f46e9217d55a482d46e8
cold-snowflake <cold.snowflake01@gmail.com>
2023-09-28 23:34:07 +0200

    tests parser

--- app/tests/parsers.py
+++ app/tests/parsers.py
@@ -1,12 +1,12 @@
 from unittest.mock import MagicMock
-
-from currency import consts
-from currency.choices import RateCurrencyChoices
+from currency.tasks import parse_privatbank, parse_monobank
 from currency.models import Rate, Source
-from currency.tasks import parse_privatbank
+from currency.choices import RateCurrencyChoices
+from currency import consts
+import json
 
 
-def test_privatbank_parser(mocker):
+def test_parse_privatbank(mocker):
     privatbank_data = [
         {"ccy": "EUR", "base_ccy": "UAH", "buy": "39.43920", "sale": "42.01681"},
         {"ccy": "USD", "base_ccy": "UAH", "buy": "36.56860", "sale": "37.45318"},
@@ -14,11 +14,13 @@ def test_privatbank_parser(mocker):
     ]
     request_get_mock = mocker.patch(
         'requests.get',
-        return_value=MagicMock(json=lambda: privatbank_data))
+        return_value=MagicMock(json=lambda: privatbank_data)
+        )
 
-    initial_count = Rate.objects.count()
+    intial_count = Rate.objects.count()
     parse_privatbank()
-    assert Rate.objects.count() == initial_count + 2
+
+    assert Rate.objects.count() == intial_count + 2
     assert request_get_mock.call_count == 1
     assert request_get_mock.call_args[0][0] == 'https://api.privatbank.ua/p24api/pubinfo?exchange&coursid=11'
 
@@ -30,19 +32,49 @@ def test_privatbank_parser_prevent_duplicates(mocker):
         {"ccy": "PLN", "base_ccy": "UAH", "buy": "36.56860", "sale": "37.45318"}
     ]
     request_get_mock = mocker.patch(
-        "requests.get", return_value=MagicMock(json=lambda: privatbank_data)
+        'requests.get',
+        return_value=MagicMock(json=lambda: privatbank_data)
     )
-    source = Source.objects.get(dev_name=consts.PRIVATBANK_DEV_NAME)
-    Rate.objects.create(
-        buy="39.62", sell="39.62", source=source, currency=RateCurrencyChoices.EUR
-    )
-    Rate.objects.create(
-        buy="37.45", sell="37.45", source=source, currency=RateCurrencyChoices.USD
-    )
+    source = Source.objects.get(code_name=consts.PRIVATBANK_CODE_NAME)
+    Rate.objects.create(buy='39.43', sell='42.01', source=source, currency=RateCurrencyChoices.EUR)
+    Rate.objects.create(buy='36.56', sell='37.45', source=source, currency=RateCurrencyChoices.USD)
 
-    rate_count = Rate.objects.count()
+    intial_count = Rate.objects.count()
 
     parse_privatbank()
 
-    assert Rate.objects.count() == rate_count
+    assert Rate.objects.count() == intial_count + 2
+
+
+with open("app/tests/fixtures/monobank.json", "r") as f:
+    monobank_data = json.load(f)
+
+
+def test_parse_monobank(mocker):
+    request_get_mock = mocker.patch(
+        'requests.get',
+        return_value=MagicMock(json=lambda: monobank_data)
+        )
+
+    intial_count = Rate.objects.count()
+    parse_monobank()
+
+    assert Rate.objects.count() == intial_count + 2
     assert request_get_mock.call_count == 1
+    assert request_get_mock.call_args[0][0] == 'https://api.monobank.ua/bank/currency'
+
+
+def test_monobank_parser_prevent_duplicates(mocker):
+    request_get_mock = mocker.patch(
+        'requests.get',
+        return_value=MagicMock(json=lambda: monobank_data)
+    )
+    source = Source.objects.get(code_name=consts.MONO_BANK_CODE_NANE)
+    Rate.objects.create(buy='39.43', sell='42.01', source=source, currency=RateCurrencyChoices.EUR)
+    Rate.objects.create(buy='36.56', sell='37.45', source=source, currency=RateCurrencyChoices.USD)
+
+    intial_count = Rate.objects.count()
+
+    parse_monobank()
+
+    assert Rate.objects.count() == intial_count + 2
